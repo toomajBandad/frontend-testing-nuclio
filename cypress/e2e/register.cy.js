f16af051c20583d3b207eacd786af0006e7d667b
toomajbandad <tbandad@gmail.com>
2025-05-19 21:05:28 +0200

    toomajbandad3

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,28 +1,44 @@
-describe('Register Form', () => {
-    const randomEmail = `user${Date.now()}@example.com`;
+describe("Register Form", () => {
+  const randomEmail = `user${Date.now()}@example.com`;
 
-    it('should register a new user successfully', () => {
-        // TODO 03
-        // Visitar la ruta /register
+  it("should register a new user successfully", () => {
+    // TODO 03
 
-        // Rellenar el formulario con un nombre, apellido, email y contraseña
-        // Utilizando el email aleatorio definido arriba
+    // Visitar la ruta /register
+    cy.visit("/register");
 
-        // Hacer clic en el botón de enviar
+    // Rellenar el formulario con un nombre, apellido, email y contraseña
+    // Utilizando el email aleatorio definido arriba
+    cy.get("input.firstName").type("toomaj");
+    cy.get("input.lastName").type("bandad");
+    cy.get("input.email").type(randomEmail);
+    cy.get("input.password").type("Password123456");
 
-        // Comprobar que la URL contiene /profile
-    });
+    // Hacer clic en el botón de enviar
+    cy.contains("Registrar").click();
 
-    it('should show an error message if the user is already registered', () => {
-        // TODO 04
-        // Visitar la ruta /register
+    // Comprobar que la URL contiene /profile
+    cy.url().should("include", "/profile");
+  });
 
-        // Rellenar el formulario con un nombre, apellido, email y contraseña
-        // Utilizando el email aleatorio definido arriba OTRA VEZ
-        // Para registrar ese mismo usuario por segunda vez
+  it("should show an error message if the user is already registered", () => {
+    // TODO 04
 
-        // Hacer clic en el botón de enviar
+    // Visitar la ruta /register
+    cy.visit("/register");
 
-        // Comprobar que se muestra el mensaje de error 'El usuario ya está registrado'
-    });
+    // Rellenar el formulario con un nombre, apellido, email y contraseña
+    // Utilizando el email aleatorio definido arriba OTRA VEZ
+    cy.get("input.firstName").type("jack");
+    cy.get("input.lastName").type("tailwind");
+    cy.get("input.email").type(randomEmail);
+    cy.get("input.password").type("Psaport123456");
+
+    // Para registrar ese mismo usuario por segunda vez
+    // Hacer clic en el botón de enviar
+    cy.contains("Registrar").click();
+
+    // Comprobar que se muestra el mensaje de error 'El usuario ya está registrado'
+    cy.get("p.errorText").contains("El usuario ya está registrado");
+  });
 });
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,31 +1,49 @@
-import {useForm} from "react-hook-form";
-import {useRegister} from "../hooks/useRegister";
-import {useNavigate} from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useRegister } from "../hooks/useRegister";
+import { useNavigate } from "react-router-dom";
 
 const RegisterForm = () => {
-    const navigate = useNavigate();
-    const {register, handleSubmit} = useForm();
-    const {register: registerUser, error, loading} = useRegister();
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+  const { register: registerUser, error, loading } = useRegister();
 
-    const onSubmit = async (data) => {
-        const result = await registerUser(data);
-        if (result) {
-            navigate('/profile');
-        }
-    };
+  const onSubmit = async (data) => {
+    const result = await registerUser(data);
+    if (result) {
+      navigate("/profile");
+    }
+  };
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("firstName")} placeholder="Nombre"/>
-            <input {...register("lastName")} placeholder="Apellido"/>
-            <input {...register("email")} placeholder="Email" type="email"/>
-            <input {...register("password")} placeholder="Contraseña" type="password"/>
-            <button type="submit" disabled={loading}>
-                {loading ? "Registrando..." : "Registrar"}
-            </button>
-            {error && <p>{error}</p>}
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        {...register("firstName")}
+        placeholder="Nombre"
+        className="firstName"
+      />
+      <input
+        {...register("lastName")}
+        placeholder="Apellido"
+        className="lastName"
+      />
+      <input
+        {...register("email")}
+        placeholder="Email"
+        type="email"
+        className="email"
+      />
+      <input
+        {...register("password")}
+        placeholder="Contraseña"
+        type="password"
+        className="password"
+      />
+      <button type="submit" disabled={loading}>
+        {loading ? "Registrando..." : "Registrar"}
+      </button>
+      {error && <p className="errorText">{error}</p>}
+    </form>
+  );
 };
 
 export default RegisterForm;
